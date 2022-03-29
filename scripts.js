f3fbc656982c0e19cadc2bb410453ef81646fe5a
theColorDude <theColorDude@outlook.com>
2022-03-29 16:59:40 +0300

    Create function to generate computer's choice

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 // Rock Paper Scissors
 
+// Randomly return 1 of the 3 predefined strings
+function computerPlay() {
+    randomNum = Math.floor(Math.random() * 3);
+
+    if (randomNum === 0) {
+        return "Rock";
+    } else if (randomNum === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+    
+}
+
+console.log('Computer played: ' + computerPlay());
