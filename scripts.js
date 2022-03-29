7d6ebc65998a79acb231769d1f5bf9a31fb5c574
theColorDude <theColorDude@outlook.com>
2022-03-29 18:16:33 +0300

    Create function to compare and return the outcome

--- scripts.js
+++ scripts.js
@@ -5,13 +5,56 @@ function computerPlay() {
     randomNum = Math.floor(Math.random() * 3);
 
     if (randomNum === 0) {
-        return "Rock";
+        return "rock";
     } else if (randomNum === 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
     
 }
 
-console.log('Computer played: ' + computerPlay());
+// Function that takes player and computer selection,
+// compares them and returns the winner
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+
+        return "Draw!";
+
+    } else if (playerSelection === "rock") {
+        
+        if (computerSelection === "paper") {
+            return "You loose! Paper beats Rock";
+        } else {
+            return "You win! Rock beats Scissors";
+        }
+
+    } else if (playerSelection === "paper") {
+        
+        if (computerSelection === "rock") {
+            return "You win! Paper beats Rock";
+        } else {
+            return "You loose! Scissors beat Paper";
+        }
+
+    } else if (playerSelection === "scissors") {
+        
+        if (computerSelection === "rock") {
+            return "You loose! Rock beats scissors";
+        } else {
+            return "You win! Scissors beat Paper";
+        }
+    } else {
+        return "Something went wrong";
+    }
+}
+
+const playerSelection = "roCk";
+const computerSelection = computerPlay();
+
+console.log("You played: " + playerSelection);
+console.log("Computer played: " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
